73c3c911a2db1974773b9a2ad3c8535d97c715d8
Rodrigo <Rodrigo.gerzur@educa.jcyl.es>
2021-11-05 10:43:23 +0100

    5/11/2021 CONF

---
+++ config/confDBMySQL.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define("HOST","192.168.3.114");
+define("PASSWORD", "P@ssw0rd");
+define("USER","usuarioDAW2103DBDepartamentos");
+define("DB", "DAW2103DBDepartamentos");
+
---
+++ config/confDBPDO.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define("HOST","mysql:host=192.168.103;dbname=DAW2103DBDepartamentos");
+define("PASSWORD", "P@ssw0rd");
+define("USER","usuarioDAW2103DBDepartamentos");
