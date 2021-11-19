65dea4332086c9da92f7506343312fec281ba68e
Rodrigo <Rodrigo.gerzur@educa.jcyl.es>
2021-11-19 13:52:04 +0100

    19/11/2021 versionado

---
+++ scriptDB/CreaDB203DWESProyectoTema41&1.php
@@ -0,0 +1,37 @@
+
+<?php
+
+try {
+
+    $miDB = new PDO(HOST, USER, PASSWORD);
+    $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = <<<EOD
+    
+    
+create database dbs4868791;
+use dbs4868791;
+
+//create user 'dbu2267458'@'%' IDENTIFIED BY 'paso';
+//grant all privileges on dbs4868791.* to 'dbu2267458'@'%' with grant option;
+
+CREATE TABLE IF NOT EXISTS Departamento(
+    CodDepartamento varchar(3) PRIMARY KEY,
+    DescDepartamento varchar(255) NOT NULL,
+    FechaBaja date NULL,
+    VolumenNegocio float NULL
+)engine=innodb; 
+ EOD;
+
+    $miDB->exec($sql);
+} catch (PDOException $excepcion) {//Codigo que se ejecuta si hay algun error
+    $errorExcepcion = $excepcion->getCode(); //Obtengo el codigo del error y lo almaceno en la variable errorException
+    $mensajeException = $excepcion->getMessage(); //Obtengo el mensaje del error y lo almaceno en la variable mensajeException
+
+    echo "<p style='color: red'>Codigo del error: </p>" . $errorExcepcion; //Muestro el codigo del error
+    echo "<p style='color: red'>Mensaje del error: </p>" . $mensajeException; //Muestro el mensaje del error
+} finally {
+    //Cierro la conexion
+    unset($miDB);
+}
+?>
